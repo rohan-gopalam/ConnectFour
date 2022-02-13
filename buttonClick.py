dec139975ec464996758234625653a8d3977181c
rohan-gopalam <88057525+rohan-gopalam@users.noreply.github.com>
2022-02-13 12:33:43 -0600

    Update buttonClick.py
    added ai boolean to implement minimax

--- buttonClick.py
+++ buttonClick.py
@@ -1,12 +1,13 @@
 from nextTurn import nextturn
 from checkWin import checkwin
+from mmai import aimove
 
 
 def buttonclick(col):
-    global positions, turn, color, buttons
+    global positions, turn, color, buttons, ai, depth
     place = False
     for k in range(6):
-        if positions[5 - k][col] == " " and place == False:
+        if positions[5 - k][col] == " " and place is False:
             positions[5 - k][col] = turn
             buttons[5 - k][col].configure(bg=color)
             place = True
@@ -17,6 +18,20 @@ def buttonclick(col):
         winner = checkwin(positions)
         if winner[1]:
             print(winner[0])
+        if ai:
+            bestPlay = aimove(positions, depth)
+            place = False
+            for row in range(6):
+                if positions[5 - row][bestPlay] == " " and place is False:
+                    positions[5 - row][bestPlay] = turn
+                    buttons[5 - row][bestPlay].configure(bg=color)
+                    place = True
+            nt = nextturn(turn, color)
+            turn = nt[0]
+            color = nt[1]
+            winner = checkwin(positions)
+            if winner[1]:
+                print(winner[0])
         #for row in range(6):
             #print(positions[row])
 
@@ -30,3 +45,5 @@ positions = [[" ", " ", " ", " ", " ", " ", " "],
 turn = "X"
 color = 'red'
 buttons = [[], [], [], [], [], []]
+ai = True
+depth = 6
