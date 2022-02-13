dccb4f25c3cf49fee73fd018368363f09d27f8ec
rohan-gopalam <88057525+rohan-gopalam@users.noreply.github.com>
2022-02-13 12:18:48 -0600

    Update checkWin.py
    fixed issue where the  checkWin function only would check if "X" won and would sometimes give IndexBound errors

--- checkWin.py
+++ checkWin.py
@@ -1,38 +1,31 @@
 def checkwin(p):
     players = ["X", "O"]
-    count = 0
+    boardHeight = len(p[0])
+    boardWidth = len(p)
+    for tile in players:
+        # check horizontal spaces
+        for y in range(boardHeight):
+            for x in range(boardWidth - 3):
+                if p[x][y] == tile and p[x+1][y] == tile and p[x+2][y] == tile and p[x+3][y] == tile:
+                    return tile, True
 
-    for play in players:
-        # horizontal check
-        for r in range(6):
-            for c in range(7):
-                if p[r][c] == play:
-                    count += 1
-                    if count == 4:
-                        return play, True
-                else:
-                    count = 0
-        # vertical check
-        for c in range(7):
-            for r in range(6):
-                if p[r][c] == play:
-                    count += 1
-                    if count == 4:
-                        return play, True
-                else:
-                    count = 0
+        # check vertical spaces
+        for x in range(boardWidth):
+            for y in range(boardHeight - 3):
+                if p[x][y] == tile and p[x][y+1] == tile and p[x][y+2] == tile and p[x][y+3] == tile:
+                    return tile, True
+
         # check / diagonal spaces
-        for x in range(7 - 3):
-            for y in range(3, 6):
-                if p[x][y] == play and p[x + 1][y - 1] == play and p[x + 2][y - 2] == play and \
-                        p[x + 3][y - 3] == play:
-                    return play, True
+        for x in range(boardWidth - 3):
+            for temp in range(boardHeight - 3):
+                y = temp + 3
+                if p[x][y] == tile and p[x+1][y-1] == tile and p[x+2][y-2] == tile and p[x+3][y-3] == tile:
+                    return tile, True
 
         # check \ diagonal spaces
-        for x in range(7 - 3):
-            for y in range(6 - 3):
-                if p[x][y] == play and p[x + 1][y + 1] == play and p[x + 2][y + 2] == play and \
-                        p[x + 3][y + 3] == play:
-                    return play, True
+        for x in range(boardWidth - 3):
+            for y in range(boardHeight - 3):
+                if p[x][y] == tile and p[x+1][y+1] == tile and p[x+2][y+2] == tile and p[x+3][y+3] == tile:
+                    return tile, True
 
     return "no one", False
